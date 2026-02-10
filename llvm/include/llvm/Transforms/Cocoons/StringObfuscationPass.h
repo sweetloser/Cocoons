0f225b1aabedd3071af4b2b636dcc51e64c9a4a5
sweetloser <18272160172@163.com>
2026-02-10 16:10:57 +0800

    fix: fixed the issue of string obfuscation in multi-module scenarios.

--- llvm/include/llvm/Transforms/Cocoons/StringObfuscationPass.h
+++ llvm/include/llvm/Transforms/Cocoons/StringObfuscationPass.h
@@ -10,6 +10,25 @@ using namespace llvm;
 
 namespace cocoons {
 
+// class StringObfuscationPass : public PassInfoMixin<StringObfuscationPass> {
+// public:
+//     PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM);
+
+//     static bool isEnabled();
+
+// private:
+//     struct ObfuscateEntry {
+//         GlobalVariable *GV;
+//         uint8_t Key;
+//     };
+
+// private:
+//     void processVariable(GlobalVariable *GV, std::vector<ObfuscateEntry> &ObfList, Module &M);
+//     Function* createDecodeHelper(Module &M);
+//     void createMainDecryptFunc(Module &M, std::vector<ObfuscateEntry> &ObfList);
+//     void encryptRealData(GlobalVariable *TargetGV, std::vector<ObfuscateEntry> &ObfList, Module &M);
+// };
+
 class StringObfuscationPass : public PassInfoMixin<StringObfuscationPass> {
 public:
     PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM);
@@ -17,17 +36,38 @@ public:
     static bool isEnabled();
 
 private:
-    struct ObfuscateEntry {
-        GlobalVariable *GV;
-        uint8_t Key;
-    };
+    /**
+     * @brief 处理全局变量，识别可混淆的字符串
+     * @param GV 当前全局变量
+     * @param Targets 存储所有可混淆字符串的全局变量列表
+     * @param M 当前 Module
+     */
+    void processVariable(GlobalVariable *GV, std::vector<GlobalVariable *> &Targets, Module &M);
+    
+    /** 
+     * @brief 加密字符串
+     */
+    bool encryptRealData(Module &M, GlobalVariable *TargetGV, uint8_t Key);
+    /**
+     * @brief 生成混淆元数据并写入指定 Section
+     * @param M 当前 Module
+     * @param EncryptedGV 已加密的全局字符串变量
+     * @param Len 字符串长度
+     * @param Key 混淆所使用的 XOR Key
+     */
+    void emitMetadata(Module &M, GlobalVariable *EncryptedGV, uint32_t Len, uint8_t Key);
 
-private:
-    void processVariable(GlobalVariable *GV, std::vector<ObfuscateEntry> &ObfList, Module &M);
-    Function* createDecodeHelper(Module &M);
-    void createMainDecryptFunc(Module &M, std::vector<ObfuscateEntry> &ObfList);
-    void encryptRealData(GlobalVariable *TargetGV, std::vector<ObfuscateEntry> &ObfList, Module &M);
+    /**
+     * @brief 将元数据变量标记为已使用，防止被链接器优化剔除
+     */
+    void markUsed(Module &M, GlobalVariable *GV);
+
+    /**
+     *
+     */
+    void injectDecrypter(Module &M);
 };
+
 } // namespace cocoons
 
 #endif
--- llvm/lib/Transforms/Cocoons/StringObfuscationPass.cpp
+++ llvm/lib/Transforms/Cocoons/StringObfuscationPass.cpp
@@ -2,6 +2,7 @@
 #include "llvm/ADT/StringRef.h"
 #include "llvm/IR/Analysis.h"
 #include "llvm/IR/Constant.h"
+#include "llvm/IR/DerivedTypes.h"
 #include "llvm/IR/GlobalValue.h"
 #include "llvm/IR/GlobalVariable.h"
 #include "llvm/IR/LLVMContext.h"
@@ -12,11 +13,13 @@
 #include "llvm/IR/Type.h"
 #include "llvm/IR/Value.h"
 #include "llvm/Pass.h"
+#include "llvm/Support/Alignment.h"
 #include "llvm/Support/Casting.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/Utils/ModuleUtils.h"
 #include <cstdint>
+#include <random>
 #include <vector>
 
 namespace cocoons {
@@ -32,9 +35,8 @@ bool StringObfuscationPass::isEnabled() {
     return EnableStrObf;
 }
 
-PreservedAnalyses StringObfuscationPass::run(Module &M, ModuleAnalysisManager &AM) {
-    std::vector<ObfuscateEntry> ObfList;
 
+PreservedAnalyses StringObfuscationPass::run(Module &M, ModuleAnalysisManager &AM) {
     // 1. 识别标记了 annotate("obfuscate") 的全局变量
     GlobalVariable *AnnoGV = M.getGlobalVariable("llvm.global.annotations");
     if (!AnnoGV) { return PreservedAnalyses::all(); }
@@ -42,6 +44,7 @@ PreservedAnalyses StringObfuscationPass::run(Module &M, ModuleAnalysisManager &A
     ConstantArray *AnnArr = dyn_cast<ConstantArray>(AnnoGV->getInitializer());
     if (!AnnArr) { return PreservedAnalyses::all(); }
 
+    std::vector<GlobalVariable *> Targets;
     for (unsigned int i = 0; i < AnnArr->getNumOperands(); ++i) {
         ConstantStruct *CS = dyn_cast<ConstantStruct>(AnnArr->getOperand(i));
         if (!CS) { continue; }
@@ -54,19 +57,37 @@ PreservedAnalyses StringObfuscationPass::run(Module &M, ModuleAnalysisManager &A
 
             StringRef Annotation = AnnoData->getAsString();
             if (Annotation.starts_with("obfuscate")) {
-                processVariable(TargetGV, ObfList, M);
+                processVariable(TargetGV, Targets, M);
             }
         }
     }
-
-    if (!ObfList.empty()) {
-        createMainDecryptFunc(M, ObfList);
+    if (Targets.empty()) {
+        errs() << ">>> 没有可混淆的字符串。\n";
+        return PreservedAnalyses::all();
     }
 
+    // 2. 准备随机数生成器
+    std::random_device RD;
+    std::default_random_engine Engine(RD());
+    std::uniform_int_distribution<int> Dist(1, 255);
+    errs() << "待加密：" << Targets.size() << "\n"; 
+    for (GlobalVariable *GV : Targets) {
+        ConstantDataSequential *CDS = cast<ConstantDataSequential>(GV->getInitializer());
+        uint32_t Len = CDS->getNumElements() * CDS->getElementByteSize();
+        uint8_t Key = (uint8_t)Dist(Engine);
+        errs() << "待加密：" << *GV << "====Len:" << Len << "=====Key:"<< Key << "====" << "\n"; 
+        if (encryptRealData(M, GV, Key)) {
+            // 5. 将解密所需的“线索”存入段
+            emitMetadata(M, GV, Len, Key);
+        }
+    }
+
+    injectDecrypter(M);
+    
+    // 既然我们修改了全局变量，告诉 LLVM 分析结果已失效
     return PreservedAnalyses::none();
 }
-
-void StringObfuscationPass::processVariable(GlobalVariable *GV, std::vector<ObfuscateEntry> &ObfList, Module &M) {
+void StringObfuscationPass::processVariable(GlobalVariable *GV, std::vector<GlobalVariable *> &Targets, Module &M) {
     if (!GV || !GV->hasInitializer()) return;
 
     Constant *Init = GV->getInitializer();
@@ -79,7 +100,7 @@ void StringObfuscationPass::processVariable(GlobalVariable *GV, std::vector<Obfu
             errs() << ">>> 排除：不是 i8 类型，可能是 int 数组或其它数据。\n";
             return;
         }
-        encryptRealData(GV, ObfList, M);
+        Targets.push_back(GV);
         return;
     } 
 
@@ -92,7 +113,7 @@ void StringObfuscationPass::processVariable(GlobalVariable *GV, std::vector<Obfu
                 V = CE->getOperand(0)->stripPointerCasts();
             }
             if (GlobalVariable *nextGV = dyn_cast<GlobalVariable>(V)) {
-                processVariable(nextGV, ObfList, M);
+                processVariable(nextGV, Targets, M);
             }
         }
         return;
@@ -101,140 +122,177 @@ void StringObfuscationPass::processVariable(GlobalVariable *GV, std::vector<Obfu
     // --- 探测点 C: 如果是一个指针引用 (C 指针或变量引用) ---
     if (GlobalVariable *NextGV = dyn_cast<GlobalVariable>(Stripped)) {
         errs() << ">>> [跳转] 识别到变量引用: " << NextGV->getName() << "，继续挖掘...\n";
-        processVariable(NextGV, ObfList, M); // 【递归】
+        processVariable(NextGV, Targets, M); // 【递归】
         return;
     }
     // --- 探测点 D: 如果是复杂的常量表达式 (GEP 等) ---
     if (ConstantExpr *CE = dyn_cast<ConstantExpr>(Stripped)) {
         Value *V = CE->getOperand(0)->stripPointerCasts();
         if (GlobalVariable *NextGV = dyn_cast<GlobalVariable>(V)) {
-            processVariable(NextGV, ObfList, M); // 【递归】
+            processVariable(NextGV, Targets, M); // 【递归】
         }
         return;
     }
 }
-void StringObfuscationPass::encryptRealData(GlobalVariable *TargetGV, std::vector<ObfuscateEntry> &ObfList, Module &M) {
-    if (TargetGV && TargetGV->hasInitializer()) {
-        Constant *ActualInit = TargetGV->getInitializer();
-        if (ConstantDataSequential *CDS = dyn_cast<ConstantDataSequential>(ActualInit)) {
-            // 防止重复加密
-            errs() << ">>>>>> 即将加密: " << TargetGV->getName() << " <<<<<<\n";
 
-            if (TargetGV->getSection() == "__DATA,__obf_strings") return;
+void StringObfuscationPass::emitMetadata(Module &M, GlobalVariable *EncryptedGV, uint32_t Len, uint8_t Key) {
+    LLVMContext &Ctx = M.getContext();
+    // 1. 定义结构体类型: { i8* addr, i32 len, i8 key }
+    StructType *MetaTy = StructType::getTypeByName(Ctx, "CocoonsMetaTy");
+    if (!MetaTy) {
+        MetaTy = StructType::create(Ctx, {
+            PointerType::get(Ctx, 0),
+            IntegerType::get(Ctx, 32),
+            IntegerType::get(Ctx, 8),
+        });
+        MetaTy->setName("CocoonsMetaTy");
+    }
 
-            errs() << ">>>>>> 执行加密逻辑: " << TargetGV->getName() << " <<<<<<\n";
-            
-            uint8_t key = 0xAD;
-            std::vector<uint8_t> Data;
-            for (unsigned i = 0; i < CDS->getNumElements(); ++i) {
-                uint8_t Val = CDS->getElementAsInteger(i);
-                // 最后一个 \0 不加密，否则 printf 无法结尾
-                if (i < CDS->getNumElements() - 1) {
-                    Val ^= key;
-                }
-                Data.push_back(Val);
+    // 赋值
+    Constant *Addr = ConstantExpr::getBitCast(EncryptedGV, PointerType::get(Ctx, 0));
+    Constant *CLen = ConstantInt::get(IntegerType::get(Ctx, 32), Len);
+    Constant *CKey = ConstantInt::get(IntegerType::get(Ctx, 8), Key);
+    Constant *StructVal = ConstantStruct::get(MetaTy, {Addr, CLen, CKey});
+
+    std::string MetaName = "_cocoons_meta_" + EncryptedGV->getName().str();
+    GlobalVariable *MetaGV = new GlobalVariable(M, MetaTy, false, GlobalValue::InternalLinkage, StructVal, MetaName);
+    MetaGV->setSection("__DATA,__cocoons_obs");
+
+    MetaGV->setAlignment(MaybeAlign(8));
+
+    // 将该变量加入 llvm.used，防止被 Linker 或 GlobalOpt 优化掉
+    markUsed(M, MetaGV);
+}
+
+void StringObfuscationPass::markUsed(Module &M, GlobalVariable *GV) {
+    appendToUsed(M, {cast<GlobalValue>(GV)});
+}
+
+bool StringObfuscationPass::encryptRealData(Module &M, GlobalVariable *TargetGV, uint8_t Key) {
+    if (!TargetGV || !TargetGV->hasInitializer()) {
+        return false;
+    }
+    errs() << "加密字符串：" << *TargetGV << "====key: " << Key << "\n";
+    Constant *ActualInit = TargetGV->getInitializer();
+    if (ConstantDataSequential *CDS = dyn_cast<ConstantDataSequential>(ActualInit)) {
+        // 防止重复加密
+        errs() << ">>>>>> 即将加密: " << TargetGV->getName() << " <<<<<<\n";
+
+        if (TargetGV->getSection() == "__DATA,__obf_strings") return false;
+
+        errs() << ">>>>>> 执行加密逻辑: " << TargetGV->getName() << " <<<<<<\n";
+        
+        std::vector<uint8_t> Data;
+        for (unsigned i = 0; i < CDS->getNumElements(); ++i) {
+            uint8_t Val = CDS->getElementAsInteger(i);
+            // 最后一个 \0 不加密，否则 printf 无法结尾
+            if (i < CDS->getNumElements() - 1) {
+                Val ^= Key;
             }
-
-            // 更新底层数据
-            TargetGV->setInitializer(ConstantDataArray::get(M.getContext(), Data));
-
-            // 修改内存属性：必须设为非常量并迁移到可写段
-            TargetGV->setConstant(false);
-            // 注意：iOS/macOS 正确的段名建议用小写 data
-            TargetGV->setSection("__DATA,__obf_strings");
-            // 确保对齐
-            TargetGV->setAlignment(MaybeAlign(1));
-
-            ObfList.push_back({TargetGV, key});
-            errs() << ">>>>>>> [DONE] Obfuscated: " << TargetGV->getName() << "\n";
+            Data.push_back(Val);
         }
+
+        // 更新底层数据
+        TargetGV->setInitializer(ConstantDataArray::get(M.getContext(), Data));
+
+        // 修改内存属性：必须设为非常量并迁移到可写段
+        TargetGV->setConstant(false);
+        // 注意：iOS/macOS 正确的段名建议用小写 data
+        TargetGV->setSection("__DATA,__obf_strings");
+        // 确保对齐
+        TargetGV->setAlignment(MaybeAlign(1));
+        errs() << ">>>>>>> [DONE] Obfuscated: " << TargetGV->getName() << "\n";
+
+        return true;
     }
+    return false;
 }
 
-void StringObfuscationPass::createMainDecryptFunc(Module &M, std::vector<ObfuscateEntry> &ObfList) {
+// 注入 LinkOnceODR 解密器
+void StringObfuscationPass::injectDecrypter(Module &M) {
     LLVMContext &Ctx = M.getContext();
-    IRBuilder<> Builder(Ctx);
-
-    // 1. 先造出通用的“工具函数”
-    Function *Helper = createDecodeHelper(M);
-
-    // 2. 定义主初始化函数
-    FunctionType *FTy = FunctionType::get(Type::getVoidTy(Ctx), false);
-    Function *MainFunc = Function::Create(FTy, GlobalValue::InternalLinkage, ".llvm_decrypt_strings", &M);
-    BasicBlock *Entry = BasicBlock::Create(Ctx, "entry", MainFunc);
-    Builder.SetInsertPoint(Entry);
-
-    // 3. 遍历每个被标记的字符串，直接调用 Helper
-    for (ObfuscateEntry Entry : ObfList) {
-        GlobalVariable *GV = Entry.GV;
-        uint64_t Len = cast<ConstantDataArray>(GV->getInitializer())->getType()->getNumElements();
-        if (Len <= 1) continue;
-        uint8_t Key = Entry.Key;
-
-        // 准备参数: (char*)GV, (int)Len-1, (int)Key
-        Value *DataPtr = Builder.CreateBitCast(GV, PointerType::get(Ctx, 0));
-        Value *LenVal = ConstantInt::get(Type::getInt32Ty(Ctx), Len - 1);
-        Value *KeyVal = ConstantInt::get(Type::getInt8Ty(Ctx), Key);
-
-        // 发射调用指令
-        Builder.CreateCall(Helper, {DataPtr, LenVal, KeyVal});
+    const DataLayout &DL = M.getDataLayout();
+    StructType *MetaTy = StructType::getTypeByName(Ctx, "CocoonsMetaTy");
+    if (!MetaTy) {
+        errs() << "未定义类型" << "\n";
+        return;
     }
+    Type *IntptrTy = DL.getIntPtrType(Ctx);
+    Type *Int32Ty = Type::getInt32Ty(Ctx);
+    Type *Int8Ty = Type::getInt8Ty(Ctx);
+    PointerType *PtrTy = PointerType::get(Ctx, 0);
+    uint64_t StructSize = DL.getTypeAllocSize(MetaTy);
+    errs() << "StructSize: " << StructSize << "\n";
+    // 1. 全局单例锁
+    GlobalVariable *Guard = new GlobalVariable(M, Int32Ty, false, 
+        GlobalValue::LinkOnceODRLinkage, ConstantInt::get(Int32Ty, 0), "__cocoons_dec_guard");
+    Guard->setVisibility(GlobalValue::HiddenVisibility);
+    Guard->setDSOLocal(true);
 
-    Builder.CreateRetVoid();
-    MainFunc->setLinkage(GlobalVariable::InternalLinkage);
-    // 将函数注册到 
-    appendToGlobalCtors(M, MainFunc, 0);
-}
+    // 2. Mach-O 边界符号引用
+    GlobalVariable *StartSect = new GlobalVariable(M, Int8Ty, false, GlobalValue::ExternalLinkage, nullptr, "\1section$start$__DATA$__cocoons_obs");
+    GlobalVariable *EndSect = new GlobalVariable(M, Int8Ty, false, GlobalValue::ExternalLinkage, nullptr, "\1section$end$__DATA$__cocoons_obs");
 
-Function* StringObfuscationPass::createDecodeHelper(Module &M) {
-    LLVMContext &Ctx = M.getContext();
-    Type *I32Ty = Type::getInt32Ty(Ctx);
-    Type *I8Ty = Type::getInt8Ty(Ctx);
-    Type *PtrTy = PointerType::get(Ctx, 0); // 统用指针类型
+    // 3. 创建解密函数
+    Function *DecFunc = Function::Create(FunctionType::get(Type::getVoidTy(Ctx), false), 
+        GlobalValue::LinkOnceODRLinkage, "__cocoons_runtime_decrypter", &M);
+    DecFunc->setVisibility(GlobalValue::HiddenVisibility);
+    DecFunc->setDSOLocal(true);
 
-    // 定义签名: void decode_logic(i8* data, i32 len, i8 key)
-    FunctionType *FTy = FunctionType::get(Type::getVoidTy(Ctx), {PtrTy, I32Ty, I8Ty}, false);
-    Function *Helper = Function::Create(FTy, GlobalValue::InternalLinkage, "decode_logic", &M);
+    // 4. 构建解密循环 IR
+    BasicBlock *EntryBB = BasicBlock::Create(Ctx, "entry", DecFunc);
+    BasicBlock *DoDecBB = BasicBlock::Create(Ctx, "do_dec", DecFunc);
+    BasicBlock *LoopCond = BasicBlock::Create(Ctx, "loop.cond", DecFunc);
+    BasicBlock *LoopBody = BasicBlock::Create(Ctx, "loop.body", DecFunc);
+    BasicBlock *InnerXor = BasicBlock::Create(Ctx, "inner.xor", DecFunc);
+    BasicBlock *LoopNext = BasicBlock::Create(Ctx, "loop.next", DecFunc);
+    BasicBlock *ExitBB = BasicBlock::Create(Ctx, "exit", DecFunc);
 
-    // 获取参数
-    Value *DataPtr = Helper->getArg(0);
-    Value *Len = Helper->getArg(1);
-    Value *Key = Helper->getArg(2);
+    IRBuilder<> B(EntryBB);
+    Value *IsInit = B.CreateLoad(Int32Ty, Guard);
+    B.CreateCondBr(B.CreateICmpEQ(IsInit, ConstantInt::get(Int32Ty, 0)), DoDecBB, ExitBB);
 
-    // --- 构建循环结构 (和之前类似，但现在是针对参数运行) ---
-    BasicBlock *Entry = BasicBlock::Create(Ctx, "entry", Helper);
-    BasicBlock *Cond = BasicBlock::Create(Ctx, "cond", Helper);
-    BasicBlock *Body = BasicBlock::Create(Ctx, "body", Helper);
-    BasicBlock *End = BasicBlock::Create(Ctx, "end", Helper);
+    B.SetInsertPoint(DoDecBB);
+    // 关键：将 GlobalVariable 强制转换为整数，这就是链接器填入的地址
+    Value *StartVal = B.CreatePtrToInt(StartSect, Type::getInt64Ty(Ctx));
+    Value *EndVal = B.CreatePtrToInt(EndSect, Type::getInt64Ty(Ctx));
 
-    IRBuilder<> B(Entry);
-    B.CreateBr(Cond);
-    B.SetInsertPoint(Cond);
+    B.CreateStore(ConstantInt::get(Int32Ty, 1), Guard);
+    B.CreateBr(LoopCond);
 
-    PHINode *IV = B.CreatePHI(I32Ty, 2, "i");
-    IV->addIncoming(ConstantInt::get(I32Ty, 0), Entry);
-    
-    // i < len
-    Value *Cmp = B.CreateICmpULT(IV, Len);
-    B.CreateCondBr(Cmp, Body, End);
+    B.SetInsertPoint(LoopCond);
+    PHINode *CurrAddr = B.CreatePHI(IntptrTy, 2, "curr.addr");
+    CurrAddr->addIncoming(B.CreatePtrToInt(StartSect, IntptrTy), DoDecBB);
+    B.CreateCondBr(B.CreateICmpULT(CurrAddr, B.CreatePtrToInt(EndSect, IntptrTy)), LoopBody, ExitBB);
 
-    B.SetInsertPoint(Body);
-    // 指令操作: data[i] = data[i] ^ Key
-    Value *CurPtr = B.CreateInBoundsGEP(I8Ty, DataPtr, IV);
-    Value *Val = B.CreateLoad(I8Ty, CurPtr);
-    Value *Xored = B.CreateXor(Val, Key);
-    B.CreateStore(Xored, CurPtr);
+    B.SetInsertPoint(LoopBody);
+    Value *EntryPtr = B.CreateIntToPtr(CurrAddr, PtrTy);
+    Value *StrAddr = B.CreateLoad(PtrTy, B.CreateStructGEP(MetaTy, EntryPtr, 0));
+    Value *StrLen = B.CreateLoad(Int32Ty, B.CreateStructGEP(MetaTy, EntryPtr, 1));
+    Value *DecLen = B.CreateSub(StrLen, ConstantInt::get(Int32Ty, 1));
+    Value *XorKey = B.CreateLoad(Int8Ty, B.CreateStructGEP(MetaTy, EntryPtr, 2));
+    Value *HasPayload = B.CreateICmpSGT(DecLen, ConstantInt::get(Int32Ty, 0));
 
-    // i++
-    Value *NextIV = B.CreateAdd(IV, ConstantInt::get(I32Ty, 1));
-    IV->addIncoming(NextIV, Body);
-    B.CreateBr(Cond);
+    // B.CreateCondBr(B.CreateICmpNE(StrLen, ConstantInt::get(Int32Ty, 0)), InnerXor, LoopNext);
+    B.CreateCondBr(HasPayload, InnerXor, LoopNext);
 
-    B.SetInsertPoint(End);
+    B.SetInsertPoint(InnerXor);
+    PHINode *Idx = B.CreatePHI(Int32Ty, 2, "idx");
+    Idx->addIncoming(ConstantInt::get(Int32Ty, 0), LoopBody);
+    Value *BytePtr = B.CreateGEP(Int8Ty, StrAddr, Idx);
+    B.CreateStore(B.CreateXor(B.CreateLoad(Int8Ty, BytePtr), XorKey), BytePtr);
+    Value *NextIdx = B.CreateAdd(Idx, ConstantInt::get(Int32Ty, 1));
+    Idx->addIncoming(NextIdx, InnerXor);
+    B.CreateCondBr(B.CreateICmpULT(NextIdx, DecLen), InnerXor, LoopNext);
+
+    B.SetInsertPoint(LoopNext);
+    CurrAddr->addIncoming(B.CreateAdd(CurrAddr, ConstantInt::get(IntptrTy, StructSize)), LoopNext);
+    B.CreateBr(LoopCond);
+
+    B.SetInsertPoint(ExitBB);
     B.CreateRetVoid();
 
-    Helper->setLinkage(GlobalVariable::InternalLinkage);
-    return Helper;
+    appendToGlobalCtors(M, DecFunc, 0);
 }
 
 } // namespace cocoons
